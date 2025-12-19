bcfd9f8a9f0587b771718e24283bc4948f11ae7c
HTRMC <143692436+HTRMC@users.noreply.github.com>
2025-12-19 02:22:05 +0100

    Refactor WaterSplashRingParticle to use RectangularParticle

--- src/main/java/com/dunnewortel/particulate/particles/splashes/WaterSplashRingParticle.java
+++ src/main/java/com/dunnewortel/particulate/particles/splashes/WaterSplashRingParticle.java
@@ -1,26 +1,27 @@
 package com.dunnewortel.particulate.particles.splashes;
 
+import com.dunnewortel.particulate.rendering.RectangularParticle;
+import com.dunnewortel.particulate.rendering.RectangularParticleSubmittable;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.particle.*;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 
-public class WaterSplashRingParticle extends BillboardParticle
-{
+@Environment(EnvType.CLIENT)
+public class WaterSplashRingParticle extends RectangularParticle {
 	protected final SpriteProvider provider;
 	private final float width;
 
-	WaterSplashRingParticle(ClientWorld clientWorld, double x, double y, double z, float width, SpriteProvider provider)
-	{
+	WaterSplashRingParticle(ClientWorld clientWorld, double x, double y, double z, float width, SpriteProvider provider) {
 		super(clientWorld, x, y, z, provider.getSprite(clientWorld.getRandom()));
 		gravityStrength = 0;
 		maxAge = 18;
@@ -29,69 +30,92 @@ public class WaterSplashRingParticle extends BillboardParticle
 		updateSprite(provider);
 	}
 
-	public RenderType getRenderType()
-	{
-		return RenderType.PARTICLE_ATLAS_TRANSLUCENT;
+	@Override
+	public BillboardParticle.RenderType getRenderType() {
+		return BillboardParticle.RenderType.PARTICLE_ATLAS_TRANSLUCENT;
 	}
 
 	@Override
-	public void tick()
-	{
-		super.tick();
+	public float getWidth(float tickProgress) {
+		float ageDelta = MathHelper.lerp(tickProgress, age - 1, (float) age);
+		float progress = ageDelta / (float) maxAge;
+		return width * (0.8f + 0.2f * progress);
+	}
 
+	@Override
+	public float getHeight(float tickProgress) {
+		return getWidth(tickProgress);
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
 		updateSprite(provider);
 
-		if (!world.getFluidState(BlockPos.ofFloored(x, y, z)).isIn(FluidTags.WATER))
-		{
+		if (!world.getFluidState(BlockPos.ofFloored(x, y, z)).isIn(FluidTags.WATER)) {
 			this.markDead();
 		}
 	}
 
-	public void render(VertexConsumer vertexConsumer, Camera camera, float tickDelta)
-	{
+	@Override
+	public void render(RectangularParticleSubmittable submittable, Camera camera, float tickDelta) {
 		Vec3d vec3d = camera.getPos();
 		float f = (float) (MathHelper.lerp(tickDelta, lastX, x) - vec3d.getX());
 		float g = (float) (MathHelper.lerp(tickDelta, lastY, y) - vec3d.getY());
 		float h = (float) (MathHelper.lerp(tickDelta, lastZ, z) - vec3d.getZ());
 
-		Vector3f[] vector3fs = new Vector3f[]{new Vector3f(-1.0F, 0.0F, -1.0f), new Vector3f(-1.0F, 0.0F, 1.0F), new Vector3f(1.0F, 0.0F, 1.0F), new Vector3f(1.0F, 0.0F, -1.0F)};
-		float ageDelta = MathHelper.lerp(tickDelta, age - 1, (float)age);
-		float progress = ageDelta / (float)maxAge;
-		float scale = width * (0.8f + 0.2f * progress);
+		float size = getWidth(tickDelta);
 
-		for (int k = 0; k < 4; ++k)
-		{
-			Vector3f vector3f2 = vector3fs[k];
-			vector3f2.mul(scale);
-			vector3f2.add(f, g, h);
+		// Flat quad on XZ plane (laying on water surface)
+		Vector3f[] corners = new Vector3f[]{
+			new Vector3f(-size, 0, -size),
+			new Vector3f(-size, 0, size),
+			new Vector3f(size, 0, size),
+			new Vector3f(size, 0, -size)
+		};
+
+		for (Vector3f corner : corners) {
+			corner.add(f, g, h);
 		}
 
-		float l = getMinU();
-		float m = getMaxU();
-		float n = getMinV();
-		float o = getMaxV();
-		int p = getBrightness(tickDelta);
-		vertexConsumer.vertex(vector3fs[0].x(), vector3fs[0].y(), vector3fs[0].z()).texture(m, o).color(red, green, blue, alpha).light(p);
-		vertexConsumer.vertex(vector3fs[1].x(), vector3fs[1].y(), vector3fs[1].z()).texture(m, n).color(red, green, blue, alpha).light(p);
-		vertexConsumer.vertex(vector3fs[2].x(), vector3fs[2].y(), vector3fs[2].z()).texture(l, n).color(red, green, blue, alpha).light(p);
-		vertexConsumer.vertex(vector3fs[3].x(), vector3fs[3].y(), vector3fs[3].z()).texture(l, o).color(red, green, blue, alpha).light(p);
+		float minU = getMinU();
+		float maxU = getMaxU();
+		float minV = getMinV();
+		float maxV = getMaxV();
+		int light = getBrightness(tickDelta);
+		int color = ColorHelper.fromFloats(alpha, red, green, blue);
+
+		// Top face (visible from above)
+		submittable.render(getRenderType(),
+			corners[0].x(), corners[0].y(), corners[0].z(),
+			corners[1].x(), corners[1].y(), corners[1].z(),
+			corners[2].x(), corners[2].y(), corners[2].z(),
+			corners[3].x(), corners[3].y(), corners[3].z(),
+			minU, maxU, minV, maxV,
+			color, light);
+
+		// Bottom face (visible from below)
+		submittable.render(getRenderType(),
+			corners[3].x(), corners[3].y(), corners[3].z(),
+			corners[2].x(), corners[2].y(), corners[2].z(),
+			corners[1].x(), corners[1].y(), corners[1].z(),
+			corners[0].x(), corners[0].y(), corners[0].z(),
+			minU, maxU, minV, maxV,
+			color, light);
 	}
 
 	@Environment(EnvType.CLIENT)
-	public static class Factory implements ParticleFactory<SimpleParticleType>
-	{
+	public static class Factory implements ParticleFactory<SimpleParticleType> {
 		private final SpriteProvider provider;
 
-		public Factory(SpriteProvider provider)
-		{
+		public Factory(SpriteProvider provider) {
 			this.provider = provider;
 		}
 
 		@Nullable
 		@Override
-		public Particle createParticle(SimpleParticleType SimpleParticleType, ClientWorld clientWorld, double x, double y, double z, double g, double h, double i, net.minecraft.util.math.random.Random random)
-		{
-			return new WaterSplashRingParticle(clientWorld, x, y, z, (float) g, provider);
+		public Particle createParticle(SimpleParticleType type, ClientWorld world, double x, double y, double z, double velX, double velY, double velZ, net.minecraft.util.math.random.Random random) {
+			return new WaterSplashRingParticle(world, x, y, z, (float) velX, provider);
 		}
 	}
 }
